5b0a465ec050e531041d4c5a5cfc3c221d15fc1e
Tarballwalf <nicu.pilt@gmail.com>
2020-05-26 01:54:57 +0300

    new messages

--- messages.js
+++ messages.js
@@ -1,7 +1,18 @@
 var messages = [
-    'Not Found',
-    'You seem to be lost...',
+    'Not found',
+    'This page doesn\'t exist',
     'You\'re lost',
+    'Nope',
+    ':(',
+    'D:',
+    'Search deeper maybe',
+    'This page was wiped from the face of the earth',
+    'Ǹ̵̢̛̳̲̌͌̽̌̌̈̈͒̓̊͝͠͝ö̴͍͉̝̟̹͚͔͖̦̯́̓̏̆̄͘͜͝͝t̵͈̦̦̯͖͔̩̠̣̦̳̅̌͐͘͜ ̸̧̨̨̩͙̜̲̱̼̩̠̩̺̝̹͚̮͖̈́͊̃̓͂̊̓̎̇̔̄̀̕͘f̸̲̃̓̒̊̏̒̈́̒͘͠͝õ̷̢̨̨̢̧̺̖̬̘̹͇͐͋̇̈́̏̈́̇ų̴̣̺̜̳̤͖͚͔͎͈̦͖͚͖̭̓͂͒̀̂̌̍͛͂͒̑̆͑͒͐̓̀̑ͅn̷̡̮̗̥̼̲̗̟̉̂͌́͌̉̒̒̈́̆̉̍͊̎͛͌͠͠d̴͚̂̅͊̅̒̚̚',
+    'The page you\'re trying to find no longer exists',
+    'Why are you here?',
+    'Where are we?',
+    'Lost, lost, lost',
+    '(:',
     'Hardcore Gay Yiff',
 ]
 function newMessage() {
